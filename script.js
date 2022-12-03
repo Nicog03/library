04421704a60cc0fa3e7f7057168ddcf4e5f66feb
nicolas <nicolas.lima03@hotmail.com>
2022-12-03 16:24:28 -0300

    implement button to delete book card

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ class Book {
     }
 }
 
+const xSVG = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"/></svg>'
+
 let library = []
 
 function createForm() {
@@ -122,7 +124,12 @@ function createForm() {
 
 function createBookCard() {
 
+    console.log(library)
+
     let container = document.createElement('div')
+    let topDiv = document.createElement('div')
+    let deleteBookButton = document.createElement('button')
+    deleteBookButton.innerHTML = xSVG
     let bookTitle = document.createElement('h3')
     let bookAuthor = document.createElement('h4')
     let bookPages = document.createElement('p')
@@ -169,7 +176,15 @@ function createBookCard() {
             }
         }
 
-        container.appendChild(bookTitle)
+        deleteBookButton.onclick = () => {
+            delete library[i]
+            library = library.filter(Boolean)
+            container.remove()
+        }
+
+        container.appendChild(topDiv)
+        topDiv.appendChild(bookTitle)
+        topDiv.appendChild(deleteBookButton)
         container.appendChild(bookAuthor)
         bottomSection.appendChild(bookReadStatus)
         bottomSection.appendChild(bookPages)
