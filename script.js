0530d838854d03ba93f0c0ff7c3569b3fec33898
nicolas <nicolas.lima03@hotmail.com>
2022-12-08 17:08:32 -0300

    change the appending of the background and form element to the body

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ class Book {
 const xSVG = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"/></svg>'
 
 let library = []
+let body = document.querySelector('body')
 
 function createForm() {
     let form = document.createElement('form')
@@ -103,8 +104,8 @@ function createForm() {
     
     //appending everything to page
     
-    main.appendChild(background)
-    main.appendChild(form)
+    body.appendChild(background)
+    body.appendChild(form)
 
     form.appendChild(closeButton)
     form.appendChild(titleDiv)
